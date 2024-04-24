b7a21b50c1ae64aa5dbcda3ad56b67a918a677a3
bluewolf44 <danielwest618@gmail.com>
2024-04-24 14:37:38 +1200

    fixed tests

--- Flat/src/main/java/dao/schema.sql
+++ Flat/src/main/java/dao/schema.sql
@@ -1,12 +1,12 @@
-drop table if exists payment;
-drop table if exists assigned;
-drop table if exists task;
+--drop table if exists payment;
+--drop table if exists assigned;
+--drop table if exists task;
 
-alter table if exists users
-drop constraint flat_users_fk;
+--alter table if exists users
+--drop constraint flat_users_fk;
 
-drop table if exists flat;
-drop table if exists users;
+--drop table if exists flat;
+--drop table if exists users;
 
 create table if not exists users
 (
@@ -33,7 +33,7 @@ create table if not exists flat
 alter table users
 add constraint flat_users_fk foreign key (flatID) references flat(flatID);
 
-create table task
+create table if not exists task
 (
  taskID varchar(10),
  taskName varchar(50) not null,
