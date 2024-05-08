9fc42a0f1e56d9ba0511c8571c31ee247e367b4b
brookecallon <brookecallon@gmail.com>
2024-05-08 12:12:29 +1200

    Navbar working

--- Frontend/flat-management-frontend/src/App.js
+++ Frontend/flat-management-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 //import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './welcome';
 import Login from './login';
 import SignUp from './signup';
@@ -8,6 +9,9 @@ import LoginSuccess from './loginSuccess';
 import CreateFlat from './createFlat';
 import JoinFlat from './joinFlat';
 import Calendar from './calendar';
+import Navbar from './navbar';
+import Tasks from './tasks';
+import Payments from './payments';
 
 function App() {
   const [view, setView] = useState('welcome');
@@ -15,7 +19,10 @@ function App() {
   const [flat, setFlat] = useState(null);
   const [tasks, setTask] = useState([]);
 
-  const handleShowLogin = () => setView('login');
+  const handleShowLogin = () => {
+    console.log("Login button clicked");
+    setView('login');
+  }
   const handleShowSignup = () => setView('signup');
   const handleSignupSuccess = () => setView('welcome');
   const handleSendJoinFlat = () => setView('createFlat');
@@ -23,6 +30,8 @@ function App() {
   const handleJoinFlat = (flatData) => {
     setFlat(flatData);
     console.log("In handle",  flatData);
+    setFlat(flatData)
+    console.log("In handle", flatData);
     setView('loginSuccess');
   }
 
@@ -36,6 +45,9 @@ function App() {
     setUser(userData);
 
     console.log("In handle",  userData);
+    // setView('loginSuccess');
+    console.log("In handle", userData);
+    console.log(userData.flatID);
     if (userData.flatID === null) {
       setView('JoinFlat');
     } else {
@@ -51,21 +63,67 @@ function App() {
     setView('loginSuccess');
   }
 
+
+  let Component;
+  switch (view) {
+    case 'welcome':
+      Component = <Welcome onShowLogin={handleShowLogin} onShowSignup={handleShowSignup} />;
+      break;
+    case 'login':
+      Component = <Login onLoginSuccess={handleLoginSuccess} />;
+      break;
+    case 'signup':
+      Component = <SignUp onSignUpSuccess={handleSignupSuccess} />;
+      break;
+    case 'loginSuccess':
+      Component = <LoginSuccess user={user} />;
+      break;
+    case 'createFlat':
+      Component = <CreateFlat onCreateSuccess={handleCreateFlat} userData={user} />;
+      break;
+    case 'JoinFlat':
+      Component = <JoinFlat createFlat={handleSendJoinFlat} joinFlat={handleJoinFlat} userData={user} />;
+      break;
+    default:
+      Component = null;
+      break;
+  }
+
   return (
-   
+
     <div className="App">
-      {view === 'welcome' && <Welcome onShowLogin={handleShowLogin} onShowSignup={handleShowSignup}/>}
+      {/* {view === 'welcome' && <Welcome onShowLogin={handleShowLogin} onShowSignup={handleShowSignup}/>} */}
+
+
+      <Router>
+
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={Component} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/payments" element={<Payments />} />
+        </Routes>
+
+      </Router>
+
+      {/* 
+      {view === 'welcome' && <Welcome onShowLogin={handleShowLogin} onShowSignup={handleShowSignup} />}
       {view === 'login' && <Login onLoginSuccess={handleLoginSuccess} />}
       {view === 'signup' && <SignUp onSignUpSuccess={handleSignupSuccess} />}
       {view === 'loginSuccess' && <LoginSuccess user={user} onCalendarTest = {handleCalendarTest}/>}
       {view === 'createFlat' && < CreateFlat onCreateSuccess = {handleCreateFlat} userData = {user}/>}
       {view === 'JoinFlat' && <JoinFlat createFlat = {handleSendJoinFlat} joinFlat = {handleJoinFlat} userData = {user}/>}
       {view === 'calendar' && <Calendar tasksData = {tasks} />}
+      {view === 'JoinFlat' && <JoinFlat createFlat = {handleSendJoinFlat} joinFlat = {handleJoinFlat} userData = {user}/>} */}
+
+
     </div>
-     
+
   );
 
 
-}
+    }
 
 export default App;
