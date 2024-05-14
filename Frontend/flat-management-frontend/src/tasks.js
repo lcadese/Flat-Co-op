7ba47a80b1f5ca4ba6552f8fda211478ba69d76f
bluewolf44 <danielwest618@gmail.com>
2024-05-14 23:39:58 +1200

    created basic html for task

--- Frontend/flat-management-frontend/src/tasks.js
+++ Frontend/flat-management-frontend/src/tasks.js
@@ -1,3 +1,28 @@
 export default function Tasks(){
-    return <h1>Add a Task</h1>
+    function test(event){
+        console.log(event.target.value)
+    }
+
+
+    return (
+        <div>
+            <h1>Add a Task</h1>
+            <form>
+                <label>Name: </label>
+                <input type="text" required />
+                <label>Description: </label>
+                <input type="text" required />
+                <label>Date: </label>
+                <input type="date" required />
+                <button type="submit">Create Task</button>
+                <label for="cars">Add People:</label>
+                <select id="people" onChange={test}>
+                  <option value="1">Example1</option>
+                  <option value="2">Example2</option>
+                  <option value="3">Example3</option>
+                  <option value="4">Example4</option>
+                </select>
+            </form>
+        </div>
+    )
 }
