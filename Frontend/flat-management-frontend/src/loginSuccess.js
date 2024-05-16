d88265d4f91f1415fe334b365f6082401437185f
bluewolf44 <danielwest618@gmail.com>
2024-05-16 22:38:57 +1200

    Tasks shown on dashboard

--- Frontend/flat-management-frontend/src/loginSuccess.js
+++ Frontend/flat-management-frontend/src/loginSuccess.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 const LoginSuccess = ({ user, onCalendarTest }) => {
   const [userData, setUserData] = useState(null);
   const [error, setError] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [tasksDisplay, setTasksDisplay] = useState([]);
 
   useEffect(() => {
     if (user && user.userID) {
@@ -11,8 +13,26 @@ const LoginSuccess = ({ user, onCalendarTest }) => {
         try {
           const response = await axios.get(`http://localhost:8080/user/${user.userID}`);
           setUserData(response.data);
+
+          const response2 = await axios.get(`http://localhost:8080/assigned/user/${user.userID}`);
+
+          const temp = [];
+          const temp2 = [];
+          for(let i=0; i < response2.data.length ; i++)
+          {
+            const response3 = await axios.get(`http://localhost:8080/tasks/${response2.data[i].taskID}`);
+            console.log(response3.data);
+            temp.push(<div>
+                <h2>{response3.data.taskName}: </h2>
+                <h3>{response3.data.description}</h3>
+                <h3>by:{response3.data.requestedDate.substring(0,10)}</h3>
+            </div>);
+            temp2.push(response3.data);
+          }
+          setTasksDisplay(temp);
+          setTasks(temp2);
         } catch (error) {
-          setError('Failed to fetch user data');
+          setError('Failed to fetch user data '+error);
         }
       };
 
@@ -48,6 +68,8 @@ const LoginSuccess = ({ user, onCalendarTest }) => {
       )}
       {error && <p>{error}</p>}
       <button onClick={handleCalendar}>Show Calendar</button>
+      <h1>Current tasks:</h1>
+      {tasksDisplay}
     </div>
   );
 };
