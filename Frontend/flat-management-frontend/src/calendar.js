52bcd3ce31bbefe7c820005de17f056afe6d7d4c
lcadese <lcadese@gmail.com>
2024-05-20 20:52:47 +1200

    Ability to hover on tasks on calander
    if tasks were too long then you couldn't see who needed to do them

--- Frontend/flat-management-frontend/src/calendar.js
+++ Frontend/flat-management-frontend/src/calendar.js
@@ -1,22 +1,41 @@
-// calendar.js
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { Tooltip } from 'react-tooltip';
 
 function test(tasks) {
   const events = tasks.map(task => ({
     title: `${task.taskName} - ${task.assignedUsers}`,
-    date: task.requestedDate
+    date: task.requestedDate.substring(0, 10),
+    extendedProps: {
+      description: task.taskName,
+      assignedUsers: task.assignedUsers
+    },
+    id: task.taskID
   }));
   return events;
 }
 
 const Calendar = ({ tasksData }) => {
+  const eventContent = (arg) => {
+    return (
+      <>
+        <div data-tooltip-id={`tooltip-${arg.event.id}`} data-tooltip-content={`${arg.event.extendedProps.description} - ${arg.event.extendedProps.assignedUsers}`}>
+          <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+            {arg.event.title}
+          </div>
+        </div>
+        <Tooltip id={`tooltip-${arg.event.id}`} place="top" effect="solid" />
+      </>
+    );
+  };
+
   return (
     <FullCalendar
       plugins={[dayGridPlugin]}
       initialView="dayGridMonth"
       events={test(tasksData)}
+      eventContent={eventContent}
     />
   );
 };
